4a2d1d4abd94e90ee85a45630c8924484e3be774
Anders Brander <anders@brander.dk>
2015-11-29 01:36:50 +0100

    Added monitor helper functions.

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -1,6 +1,7 @@
 package monitor
 
 import (
+	"errors"
 	"math/rand"
 	"os"
 	"sync"
@@ -24,23 +25,74 @@ type (
 	}
 )
 
-func Loop(wg sync.WaitGroup) {
+var (
+	sess       *mgo.Session
+	db         *mgo.Database
+	collection *mgo.Collection
+
+	ErrorInvalidId error = errors.New("Invalid id")
+)
+
+func init() {
 	sess, err := mgo.Dial("127.0.0.1")
 	if err != nil {
 		logger.Error("monitor", "Can't connect to mongo, go error %v\n", err)
 		os.Exit(1)
 	}
-	defer sess.Close()
 
-	collection := sess.DB("alerto").C("monitors")
+	db = sess.DB("alerto")
+	collection = db.C("monitors")
+}
 
+func GetAllMonitors() []Monitor {
+	var monitors []Monitor
+
+	err := collection.Find(bson.M{}).All(&monitors)
+	if err != nil {
+		logger.Red("monitor", "Error getting monitors from Mongo: %s", err.Error())
+	}
+
+	return monitors
+}
+
+func GetMonitor(id string) (Monitor, error) {
+	var monitor Monitor
+
+	if !bson.IsObjectIdHex(id) {
+		return monitor, ErrorInvalidId
+	}
+
+	err := collection.FindId(bson.ObjectIdHex(id)).One(&monitor)
+	if err != nil {
+		logger.Red("monitor", "Error getting monitors from Mongo: %s", err.Error())
+		return monitor, err
+	}
+
+	return monitor, nil
+}
+
+func UpdateMonitor(mon *Monitor) error {
+	return collection.UpdateId(mon.Id, mon)
+}
+
+func AddMonitor(mon *Monitor) error {
+	mon.Id = bson.NewObjectId()
+
+	return collection.Insert(mon)
+}
+
+func DeleteMonitor(mon *Monitor) error {
+	return collection.RemoveId(mon.Id)
+}
+
+func Loop(wg sync.WaitGroup) {
 	ticker := time.Tick(time.Millisecond * 100)
 
 	inFlight := make(map[bson.ObjectId]bool)
 	inFlightLock := sync.RWMutex{}
 	for t := range ticker {
 		var monitors []Monitor
-		err = collection.Find(bson.M{}).All(&monitors)
+		err := collection.Find(bson.M{}).All(&monitors)
 		if err != nil {
 			logger.Red("monitor", "Error getting monitors from Mongo: %s", err.Error())
 			continue
