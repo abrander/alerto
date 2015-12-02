fe6bf120cc7d28e2502f2230e7fa9acad2c87fa3
Anders Brander <anders@brander.dk>
2015-12-02 22:47:08 +0100

    Added key generation to ssh plugin.

--- plugins/ssh/ssh.go
+++ plugins/ssh/ssh.go
@@ -1,11 +1,18 @@
 package ssh
 
 import (
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"io/ioutil"
+	"path"
 
 	"golang.org/x/crypto/ssh"
 
+	"github.com/abrander/alerto/config"
 	"github.com/abrander/alerto/logger"
 )
 
@@ -17,6 +24,85 @@ type (
 	}
 )
 
+const (
+	privateKeyFilename = "id_rsa"
+	publicKeyFilename  = "id_rsa.pub"
+)
+
+var (
+	signer    ssh.Signer
+	PublicKey string
+)
+
+func GenerateKey() ([]byte, error) {
+	var pemBuffer bytes.Buffer
+
+	// Generate RSA keypair
+	rsaKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, err
+	}
+
+	// Encode RSA private key to pem
+	pem.Encode(&pemBuffer, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(rsaKey),
+	})
+
+	err = ioutil.WriteFile(path.Join(config.ConfigDir, privateKeyFilename), pemBuffer.Bytes(), 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	return pemBuffer.Bytes(), nil
+}
+
+func init() {
+	pemBytes, err := ioutil.ReadFile(path.Join(config.ConfigDir, privateKeyFilename))
+	if err != nil {
+		pemBytes, err = GenerateKey()
+		if err != nil {
+			logger.Error("ssh", err.Error())
+		}
+	}
+
+	// Parse private key for ssh
+	signer, err = ssh.ParsePrivateKey(pemBytes)
+	if err != nil {
+		logger.Error("ssh", err.Error())
+	}
+
+	// Parse private key for generating public key
+	key, err := ssh.ParseRawPrivateKey(pemBytes)
+	if err != nil {
+		logger.Error("ssh", err.Error())
+		return
+	}
+
+	// Convert to RSA key
+	rsaKey, ok := key.(*rsa.PrivateKey)
+	if !ok {
+		logger.Error("ssh", "Wrong key file format")
+		return
+	}
+
+	// Generate public key (this is deterministic)
+	rsaPubKey, err := ssh.NewPublicKey(&rsaKey.PublicKey)
+	if err != nil {
+		logger.Error("ssh", err.Error())
+		return
+	}
+
+	// Generate nice string for ~/.ssh/authorized_keys
+	PublicKey = string(bytes.TrimSpace(ssh.MarshalAuthorizedKey(rsaPubKey))) + " https://github.com/abrander/alerto\n"
+
+	// Write file for convenience and automation
+	err = ioutil.WriteFile(path.Join(config.ConfigDir, publicKeyFilename), []byte(PublicKey), 0644)
+	if err != nil {
+		logger.Error("ssh", err.Error())
+	}
+}
+
 func (s *Ssh) Connect() (*ssh.Client, error) {
 	// FIXME: Support default
 	if s.Port == 0 {
@@ -26,15 +112,6 @@ func (s *Ssh) Connect() (*ssh.Client, error) {
 	dialString := fmt.Sprintf("%s:%d", s.Host, s.Port)
 	logger.Yellow("ssh", "Connecting to %s as %s", dialString, s.Username)
 
-	pemBytes, err := ioutil.ReadFile("/home/abrander/src/github.com/abrander/alerto/private.pem")
-	if err != nil {
-		return nil, err
-	}
-
-	signer, err := ssh.ParsePrivateKey(pemBytes)
-	if err != nil {
-		return nil, err
-	}
 	config := &ssh.ClientConfig{
 		User: s.Username,
 		Auth: []ssh.AuthMethod{ssh.PublicKeys(signer)},
