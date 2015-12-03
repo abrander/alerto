ece884f7d92952ac3835dbea2e7f23e1275be701
Anders Brander <anders@brander.dk>
2015-12-03 23:40:05 +0100

    Ported http to plugins.Transport.Dial() and implemented simple instrumentation.

--- plugins/http/http.go
+++ plugins/http/http.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"net"
 	"net/http"
 	"time"
 
@@ -19,20 +20,75 @@ type (
 	Http struct {
 		Url string `json:"url" description:"The URL to request"`
 	}
+
+	instrumentTransport struct {
+		rtp       http.RoundTripper
+		dialer    func(network, addr string) (net.Conn, error)
+		connStart time.Time
+		connEnd   time.Time
+		reqStart  time.Time
+		reqEnd    time.Time
+	}
 )
+
+func newTransport(dialer func(network, addr string) (net.Conn, error)) *instrumentTransport {
+	tr := &instrumentTransport{
+		dialer: dialer,
+	}
+
+	tr.rtp = &http.Transport{
+		Proxy:               http.ProxyFromEnvironment,
+		Dial:                tr.dial,
+		TLSHandshakeTimeout: 10 * time.Second,
+	}
+
+	return tr
+}
+
+func (tr *instrumentTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	tr.reqStart = time.Now()
+	resp, err := tr.rtp.RoundTrip(r)
+	tr.reqEnd = time.Now()
+
+	return resp, err
+}
+
+func (tr *instrumentTransport) dial(network, addr string) (net.Conn, error) {
+	tr.connStart = time.Now()
+	cn, err := tr.dialer(network, addr)
+	tr.connEnd = time.Now()
+
+	return cn, err
+}
+
+func (tr *instrumentTransport) RequestDuration() time.Duration {
+	return tr.Duration() - tr.ConnectDuration()
+}
+
+func (tr *instrumentTransport) ConnectDuration() time.Duration {
+	return tr.connEnd.Sub(tr.connStart)
+}
+
+func (tr *instrumentTransport) Duration() time.Duration {
+	return tr.reqEnd.Sub(tr.reqStart)
+}
 
 func (h *Http) Run(transport plugins.Transport, request plugins.Request) plugins.Result {
 	start := time.Now()
 
-	resp, err := http.Get(h.Url)
+	tr := newTransport(transport.Dial)
+	client := &http.Client{Transport: tr}
+	resp, err := client.Get(h.Url)
 	if err != nil {
 		return plugins.NewResult(plugins.Failed, plugins.NewMeasurementCollection("time", time.Now().Sub(start)), err.Error())
 	}
 	defer resp.Body.Close()
 
 	c := plugins.NewMeasurementCollection(
-		"time", time.Now().Sub(start),
 		"status", resp.StatusCode,
+		"time", tr.Duration(),
+		"connectDuration", tr.ConnectDuration(),
+		"requestDuration", tr.RequestDuration(),
 	)
 
 	return plugins.NewResult(plugins.Ok, c, "returned %d", resp.StatusCode)
